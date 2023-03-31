72d644a8dd3c5e0d26b5010018806a698f4ca268
rbrunob <bruno.bragaramos@gmail.com>
2023-03-31 14:01:54 -0300

    fix video scroll muted

--- src/Components/Plataform/Plataform.php
+++ src/Components/Plataform/Plataform.php
@@ -6,7 +6,7 @@
         <div class="mockup_container">
             <div class="video_and_img out-in top">
                 <div class="video_container">
-                    <video src="https://preprod.containermedia.com.br/containermediatech/src/assets/videos/video-moc-to-top.mov" autoplay="true" loop="true"></video>
+                    <video src="https://preprod.containermedia.com.br/containermediatech/src/assets/videos/video-moc-to-top.mov" autoplay="true" loop="true" muted="true"></video>
                 </div>
                 <img src="https://preprod.containermedia.com.br/containermediatech/src/assets/images/moc-video.webp" loading="lazy" alt="Celular" />
             </div>
